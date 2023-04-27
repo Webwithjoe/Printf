aa8be2edd6d19359bbc2b19130645660a40d8444
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-27 03:45:45 +0000

    case d modifications

--- main.c
+++ main.c
@@ -9,66 +9,235 @@
  */
 int main(void)
 {
+
 	int len, len2;
-	/*char *str = "You are tasked to come up with solutions for the tasks below yourself to meet with the above learning objectives.You will not be able to meet the objectives of this or any following project by copying and pasting someone else’s work.You are not allowed to publish any content of this project.Any form of plagiarism is strictly forbidden and will result in removal from the program.";
-  
-    _printf("Let's print a simple sentence.\n");
-    _printf("%c", 'S');
-    printf("\n");
-    _printf("A char inside a sentence: %c. Did it work?\n", 'F');
-    _printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
-    _printf("%s", "This sentence is retrieved from va_args!\n");
-    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-    _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-   
-    _printf("%%");
-    _printf("\n");
+	long int l;
 
-    _printf("Should print a single percent sign: %%\n");
-    _printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
-    _printf("css%ccs%scscscs", 'T', "Test");
-    _printf("\n");
-    _printf(str);
-    _printf("\n");
-    _printf(NULL);
-    _printf("%c", '\0');
-    _printf("%");
-    _printf("%!\n");
-    _printf("\n");
-      
-    _printf("%K\n");
+	len = _printf("%d", 1024);
+	printf("\n");
+	len2 = printf("%d", 1024);
+	printf("\n");
 
-    _printf("man gcc:\n%s", str);*/
-	/*len = _printf(NULL);
-	len2 = printf(NULL);
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+    
+	len = _printf("%d", -1024);
+	printf("\n");
+	len2 = printf("%d", -1024);
+	printf("\n");
 	printf("%d", len);
 	printf("\n");
 	printf("%d", len2);
 	printf("\n");
 	
-	len = _printf("%c", '\0');
-	len2 = printf("%c", '\0');
-	printf("%d", len);
+
+	
+	len = _printf("%d", 0);
 	printf("\n");
-	printf("%d", len2);
-	printf("\n");*/
-    len = _printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-    len2 = printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-	printf("%d", len);
+	len2 = printf("%d", 0);
 	printf("\n");
-	printf("%d", len2);
-	printf("\n");
-    
-    len = _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-     len2 = printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
 	printf("%d", len);
 	printf("\n");
 	printf("%d", len2);
 	printf("\n");
-    
-    len = _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-    len2 = printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+
+	len = _printf("%d", INT_MAX);
+	printf("\n");
+	len2 = printf("%d", INT_MAX);
+	printf("\n");
+	
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%d", INT_MIN);
+	printf("\n");
+	len2 = printf("%d", INT_MIN);
+	printf("\n");
+	
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	
+	l = (long int) INT_MAX + 1024;
+	
+	len = _printf("%d", l);
+	printf("\n");
+	len2 = printf("%d", l);
+	printf("\n");
+	
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	l = (long int) INT_MIN - 1024;
+	
+	len = _printf("%d", l); 
+	printf("\n");
+	len2 = printf("%d", l); 
+	printf("\n");
+	
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+
+	len = _printf("There is %d bytes in %d KB\n", 1024, 1);
+	printf("\n");
+	len2 = printf("There is %d bytes in %d KB\n", 1024, 1);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%d - %d = %d\n", 1024, 2048, -1024);
+	len2 = printf("%d - %d = %d\n", 1024, 2048, -1024);
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+
+	len = _printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	printf("\n");
+	len2 = printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%i", 1024);
+	printf("\n");
+	len2 = printf("%i", 1024);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%i", -1024);
+	printf("\n");
+	len2 = printf("%i", -1024);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+
+	len = _printf("%i", 0);
+	printf("\n");
+	len2 = printf("%i", 0);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+
+	len = _printf("%i", INT_MAX);
+	printf("\n");
+	len2 = printf("%i", INT_MAX);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%i", INT_MIN);
+	printf("\n");
+	len2 = printf("%i", INT_MIN);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	l = (long int) INT_MAX + 1024;
+	
+	len = _printf("%i", l);
+	printf("\n");
+	len2 = printf("%i", l);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	l = (long int) INT_MIN - 1024;
+	
+	len = _printf("%i", l);
+	printf("\n");
+	len2 = printf("%i", l);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("There is %i bytes in %i KB\n", 1024, 1);
+	printf("\n");
+	len2 = printf("There is %i bytes in %i KB\n", 1024, 1);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%i - %i = %i\n", 1024, 2048, -1024);
+	printf("\n");
+	len2 = printf("%i - %i = %i\n", 1024, 2048, -1024);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	printf("\n");
+	len2 = printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+
+	len = _printf("%d == %i\n", 1024, 1024);
+	printf("\n");
+	len2 = printf("%d == %i\n", 1024, 1024);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("iddi%diddiiddi\n", 1024);
+	printf("\n");
+	len2 = printf("iddi%diddiiddi\n", 1024);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%d", 10000);
+	printf("\n");
+	len2 = printf("%d", 10000);
+	printf("\n");
+	printf("%d", len);
+	printf("\n");
+	printf("%d", len2);
+	printf("\n");
+	
+	len = _printf("%i", 10000);
+	printf("\n");
+	len2 = printf("%i", 10000);
+	printf("\n");
 	printf("%d", len);
 	printf("\n");
 	printf("%d", len2);
